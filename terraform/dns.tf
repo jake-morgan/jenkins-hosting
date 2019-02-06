1ebd0f5c9fd55f2d7330e7726370e87022282be9
Jake Morgan <jakeelliotmorgan@gmail.com>
2019-02-06 13:19:39 +0000

    added java install to jenkins role

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,12 +1,8 @@
-resource "azurerm_dns_zone" "main" {
-  name                = "${var.domain_name}"
-  resource_group_name = "dns"
-}
-
 resource "azurerm_dns_a_record" "jenkins_record" {
   name                = "jenkins"
-  zone_name           = "${azurerm_dns_zone.main.name}"
-  resource_group_name = "${azurerm_dns_zone.main.resource_group_name}"
+  zone_name           = "${var.domain_name}"
+  resource_group_name = "dns"
   ttl                 = 60
   records             = ["${azurerm_public_ip.main.ip_address}"]
+  depends_on          = ["azurerm_public_ip.main"]
 }
